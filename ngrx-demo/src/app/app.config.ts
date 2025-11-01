247c1555b842589264609c4e75267ce3a947fd96
vsr1414 <kamidivijayasimhareddy@gmail.com>
2025-11-01 14:31:42 +0530

    installed ngrx store @ 20 version

--- ngrx-demo/src/app/app.config.ts
+++ ngrx-demo/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideStore()
+]
 };
